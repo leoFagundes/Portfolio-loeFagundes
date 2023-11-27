f164a5a501098cd3372291713b99d23a9c05db45
leoFagundes <leofagundes2015@gmail.com>
2023-11-27 14:59:02 -0300

    Update projects view

--- src/components/Projects/SlideProject/index.tsx
+++ src/components/Projects/SlideProject/index.tsx
@@ -17,21 +17,25 @@ export default function SlideProject({currentProjectIndex, currentProject, curre
     if (!currentProject && currentProjectSide) {
         return (
             <div className={`${currentProjectSide === 1 ? style.leftSlide : style.rightSlide} ${currentProjectIndex === 0 ? style.sideSlide : ''}`}>
-                {projectList.map(({imageName, name, des, ling, link}, index) => (
+                {projectList.map(({imageName, name, des, ling, link, demo}, index) => (
                     <div
                         key={index}
                         className={`${style.item} ${index + currentProjectSide === currentProjectIndex ? style.active : style.hidden}`}
                     >
-                        <a href={link} target='_blank' rel="noreferrer">
                             <div className={style.content}>
                                 <div className={style.image} style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/${imageName})` }}/>
                                 <div className={style.name}>
                                     {name}<br/>
                                     <i style={{fontSize: '13px', color: '#54d489'}}>{ling}</i>
                                 </div>
-                                <div className={style.description}><i>Descrição do projeto {name} oculta momentaneamente.</i></div>
+                                <div className={style.description}>
+                                    <i>Descrição do projeto {name} oculta momentaneamente.</i>
+                                    <div className={style.linksButtons}>
+                                        <a href={link} target='_blank'><button type='button'>Repo <RxGithubLogo /></button></a>
+                                        {demo ? <a href={demo} target='_blank'><button type='button'>Demo <RiExternalLinkLine /></button></a> : ''}
+                                    </div>
+                                </div>
                             </div>
-                        </a>
                     </div>
                 ))}
             </div>
@@ -44,7 +48,6 @@ export default function SlideProject({currentProjectIndex, currentProject, curre
                     key={index}
                     className={`${style.item} ${index === currentProjectIndex ? style.active : style.hidden}`}
                     >
-                        
                             <div className={style.content}>
                                 <div className={style.image} style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/${imageName})` }}/>
                                 <div className={style.name}>
