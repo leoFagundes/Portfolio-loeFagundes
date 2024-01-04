21b658ed9f4459abd3c2ca7182f56c4bff6ab557
leoFagundes <leofagundes2015@gmail.com>
2024-01-04 16:41:19 -0300

    update details

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,44 +1,51 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Title from '../Title';
 import style from './Skills.module.scss';
-import { Modal, Button } from 'react-bootstrap';
 import SkillsModal from './SkillsModal/SkillsModal';
 import { skillsData } from './skillsData';
 
 export type skillProps = {
-    name: string
-    imgSrc: string
-    descricao: string
-}
+    name: string;
+    imgSrc: string;
+    descricao: string;
+};
 
 export default function Skills() {
-    const [showModal, setShowModal] = useState(false)
-    const [modalData, setModalData] = useState<skillProps>()
+    const skillsSectionRef = useRef<HTMLDivElement | null>(null);
+    const [showModal, setShowModal] = useState(false);
+    const [modalData, setModalData] = useState<skillProps>();
 
     const handleClose = () => setShowModal(false);
     const handleShow = ({ name, imgSrc, descricao }: skillProps) => {
-        setShowModal(true)
-        setModalData({ name, imgSrc, descricao })
+        setShowModal(true);
+        setModalData({ name, imgSrc, descricao });
+
+        // Scroll into the center of the Skills section
+        if (skillsSectionRef.current) {
+            skillsSectionRef.current.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center',
+            });
+        }
     };
 
     return (
-        <section id='Skills' className={style.Skills}>
+        <section ref={skillsSectionRef} id='Skills' className={style.Skills}>
             <Title titleName='Habilidades' />
             <div className={style.containerSkills}>
                 {skillsData.map(({ name, imgSrc, descricao }, index) => (
-                    <div onClick={() => handleShow({ name, imgSrc, descricao })} key={`${index + 1} - ${name}`} className={style.skillCard}>
-                        <div style={{ backgroundImage: `url("${imgSrc}")`, backgroundPosition: "50% 50%" }} className={style.imgSkillLogo}></div>
+                    <div
+                        onClick={() => handleShow({ name, imgSrc, descricao })}
+                        key={`${index + 1} - ${name}`}
+                        className={style.skillCard}
+                    >
+                        <div style={{ backgroundImage: `url("${imgSrc}")`, backgroundPosition: '50% 50%' }} className={style.imgSkillLogo}></div>
                         <h1>{name}</h1>
                     </div>
                 ))}
             </div>
 
-            {showModal ? (
-                <SkillsModal handleClose={handleClose} modalData={modalData} />
-            ) : (
-                ''
-            )}
-
+            {showModal ? <SkillsModal handleClose={handleClose} modalData={modalData} /> : ''}
         </section>
     );
 }
--- src/components/Skills/skillsData.ts
+++ src/components/Skills/skillsData.ts
@@ -19,19 +19,6 @@ export const skillsData = [
       "JavaScript (JS) é uma linguagem de programação de alto nível que permite adicionar interatividade e comportamento dinâmico a sites e aplicativos web. É uma das principais tecnologias para o desenvolvimento web front-end.",
   },
   {
-    name: "HTML",
-    imgSrc: "assets/img/skills/html-logo.png",
-    descricao:
-      "HTML (HyperText Markup Language) é a linguagem de marcação usada para estruturar o conteúdo de páginas da web. Ele define a estrutura do documento, como cabeçalhos, parágrafos, links e elementos de formulário.",
-  },
-  {
-    name: "CSS",
-    imgSrc: "assets/img/skills/css-logo2.png",
-    descricao:
-      "CSS (Cascading Style Sheets) é usado para estilizar páginas da web, controlando a aparência e o layout dos elementos HTML. Ele permite a criação de designs atraentes e responsivos.",
-  },
-
-  {
     name: "TypeScript",
     imgSrc: "assets/img/skills/ts-logo.png",
     descricao:
@@ -44,10 +31,22 @@ export const skillsData = [
       "React é uma biblioteca JavaScript para a criação de interfaces de usuário (UI). Ela é usada para desenvolver aplicativos web interativos e reativos, facilitando a construção de componentes reutilizáveis.",
   },
   {
-    name: "SQL",
-    imgSrc: "assets/img/skills/sql-logo2.png",
+    name: "Next",
+    imgSrc: "assets/img/skills/next-logo.png",
     descricao:
-      "SQL (Structured Query Language) é uma linguagem de consulta usada para gerenciar e manipular bancos de dados relacionais. É essencial para armazenar, recuperar e gerenciar dados de forma eficiente.",
+      "Next.js é um framework para React que facilita a construção de aplicativos web React com renderização do lado do servidor, roteamento automático e muitas outras funcionalidades úteis para desenvolvimento web moderno.",
+  },
+  {
+    name: "HTML",
+    imgSrc: "assets/img/skills/html-logo.png",
+    descricao:
+      "HTML (HyperText Markup Language) é a linguagem de marcação usada para estruturar o conteúdo de páginas da web. Ele define a estrutura do documento, como cabeçalhos, parágrafos, links e elementos de formulário.",
+  },
+  {
+    name: "CSS",
+    imgSrc: "assets/img/skills/css-logo2.png",
+    descricao:
+      "CSS (Cascading Style Sheets) é usado para estilizar páginas da web, controlando a aparência e o layout dos elementos HTML. Ele permite a criação de designs atraentes e responsivos.",
   },
   {
     name: "Tailwind",
@@ -74,9 +73,9 @@ export const skillsData = [
       "SASS (Syntactically Awesome Stylesheets) é uma linguagem de extensão do CSS que adiciona funcionalidades como variáveis, aninhamento de seletores e mixins. Facilita a escrita e manutenção de estilos complexos.",
   },
   {
-    name: "Next",
-    imgSrc: "assets/img/skills/next-logo.png",
+    name: "SQL",
+    imgSrc: "assets/img/skills/sql-logo2.png",
     descricao:
-      "Next.js é um framework para React que facilita a construção de aplicativos web React com renderização do lado do servidor, roteamento automático e muitas outras funcionalidades úteis para desenvolvimento web moderno.",
+      "SQL (Structured Query Language) é uma linguagem de consulta usada para gerenciar e manipular bancos de dados relacionais. É essencial para armazenar, recuperar e gerenciar dados de forma eficiente.",
   },
 ];
